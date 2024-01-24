aa0955b25c9969b47bd3b3142370580e9af64109
Dominic Culotta <culotta2@illinois.edu>
2024-01-23 22:24:48 -0600

    Created a small AST to parse a simple query

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -1,85 +1,93 @@
 use crate::lexer::lexer;
 
-// TODO: Replace the syntax type with an enum
-// once all the syntax types are defined
-//
-// enum SyntaxType {
-//     Program,
-//     Select,
-// }
+#[allow(dead_code)]
+#[derive(Clone, Debug, PartialEq)]
+enum AstNode {
+    Program(Vec<AstNode>),
+    Statement(Vec<AstNode>),
+    Expression(lexer::Token),
+}
 
-struct SyntaxNode {}
+#[allow(dead_code)]
+impl AstNode {
+    fn add_child(&mut self, child: AstNode) {
+        match self {
+            AstNode::Program(node) | AstNode::Statement(node) => node.push(child),
+            AstNode::Expression(_) => todo!("Not sure what to do for an expression"),
+        }
+    }
+}
 
-impl SyntaxNode {
-    fn new(tokens: Vec<lexer::Token>) -> Self {
-        SyntaxNode {}
+#[allow(dead_code)]
+fn build_ast(tokens: Vec<lexer::Token>) -> AstNode {
+    let mut program_node = AstNode::Program(vec![]);
+    let mut statement_node = AstNode::Statement(vec![]);
+
+    for token in tokens {
+        let ast_node = match token {
+            lexer::Token::DML(lexer::DMLKeyword::Select) => AstNode::Expression(token),
+            lexer::Token::Ident(_) | lexer::Token::Varchar(_) => AstNode::Expression(token),
+            _ => AstNode::Expression(token), // TODO: Currently treating all other tokens as
+                                             // expressions
+        };
+
+        statement_node.add_child(ast_node.clone());
     }
-    
-    pub fn parse(lexer: lexer::Lexer) -> Vec<lexer::Token> {
-        return lexer.get_tokens().unwrap();
-    }
+
+    program_node.add_child(statement_node);
+    program_node
+}
+
+#[allow(dead_code)]
+enum Clause {
+    Select,
+    From,
+    Where,
+    // Having,
+    // Qualify,
+    GroupBy,
+    // OrderBy,
+    // Limit,
+    // Join,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::lexer::lexer;
-    use crate::ast::ast::SyntaxNode;
+    use super::*;
 
     #[test]
-    fn test_get_tokens() {
-        let input = r#"
--- Create a temporary table
-CREATE TEMPORARY TABLE temp_table (
-  id INT,
-  name VARCHAR,
-  age INT
-);
+    fn test_basic_query() {
+        // Query: SELECT date, temperate FROM table WHERE date = '2022-01-01';
+        let tokens = vec![
+            lexer::Token::DML(lexer::DMLKeyword::Select),
+            lexer::Token::Ident("date".into()),
+            lexer::Token::Comma,
+            lexer::Token::Ident("temperature".into()),
+            lexer::Token::From,
+            lexer::Token::Ident("table".into()),
+            lexer::Token::Where,
+            lexer::Token::Ident("date".into()),
+            lexer::Token::Equal,
+            lexer::Token::Varchar("2022-01-01".into()),
+            lexer::Token::Semicolon,
+        ];
 
--- Create a stored procedure
-CREATE OR REPLACE PROCEDURE my_stored_procedure()
-RETURNS VARCHAR
-LANGUAGE SQL
-AS
-$$
-  -- SQL code goes here
-  -- ...
-  RETURN 'Stored procedure executed successfully';
-$$;
+        let ast_expected = AstNode::Program(vec![AstNode::Statement(vec![
+            AstNode::Expression(lexer::Token::DML(lexer::DMLKeyword::Select)),
+            AstNode::Expression(lexer::Token::Ident("date".into())),
+            AstNode::Expression(lexer::Token::Comma),
+            AstNode::Expression(lexer::Token::Ident("temperature".into())),
+            AstNode::Expression(lexer::Token::From),
+            AstNode::Expression(lexer::Token::Ident("table".into())),
+            AstNode::Expression(lexer::Token::Where),
+            AstNode::Expression(lexer::Token::Ident("date".into())),
+            AstNode::Expression(lexer::Token::Equal),
+            AstNode::Expression(lexer::Token::Varchar("2022-01-01".into())),
+            AstNode::Expression(lexer::Token::Semicolon),
+        ])]);
 
--- Create a function
-CREATE OR REPLACE FUNCTION my_function(arg INT)
-RETURNS TABLE (id INT, name VARCHAR)
-LANGUAGE SQL
-AS
-$$
-  -- SQL code goes here
-  -- ...
-  RETURN 'Stored procedure executed successfully';
-$$;
+        let ast_actual = build_ast(tokens);
 
--- Use a CTE to query data
-WITH cte AS (
-  SELECT id, name, salary
-  FROM temp_table
-  WHERE age > 30
-)
-SELECT name, sum(salary)
-FROM cte
-group by name
-;
-
--- Call the stored procedure
-CALL my_stored_procedure();
-
--- Call the function
-SELECT *
-FROM TABLE(my_function(123));
-            "#;
-        let lexer = lexer::Lexer::new(input.into());
-        let x = SyntaxNode::parse(lexer);
-        for val in x {
-            println!("{:?}", val);
-        }
-        assert!(false);
+        assert_eq!(ast_expected, ast_actual);
     }
 }
